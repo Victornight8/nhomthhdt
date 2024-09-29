e6975f7c991d95d9eb50499428b74674a39da983
VoHoangDinh <hoangdinh150505@gmail.com>
2024-09-29 15:05:24 +0700

    baitaptuan3_456

---
+++ bai4_tuan3.java
@@ -0,0 +1,23 @@
+
+package baitap_tuan3;
+import java.util.Scanner;
+public class bai4_tuan3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.printf("Nhap vao so thu nhat: ");
+        int a = sc.nextInt();
+        System.out.printf("Nhap vao so thu hai: ");
+        int b = sc.nextInt();
+        System.out.printf("Nhap vao so thu ba: ");
+        int c = sc.nextInt();
+        if (a < b && a < c) {
+            System.out.println("so nho nhat trong ba so la: " + a);
+        } else {
+            if (b < c) {
+                System.out.println("so nho nhat trong ba so la: " + b);
+            } else {
+                System.out.println("so nho nhat trong ba so la: " + c);
+            }
+        }
+        }
+}
